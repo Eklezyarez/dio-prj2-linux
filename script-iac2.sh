31185dc216d54c6ce75ca5176be66f247d8d22e4
Ivan Carlos de Oliveira Santos <104778982+Eklezyarez@users.noreply.github.com>
2022-12-24 08:43:08 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -6,7 +6,7 @@ apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
 
-echo "Baixando e copiando os arquivos da aplicaç
+echo "Baixando e copiando os arquivos da aplica
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
